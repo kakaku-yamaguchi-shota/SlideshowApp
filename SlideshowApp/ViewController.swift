40f5fd6fe598dd2d67623d31632138b45b47512d
yamaguchi_shota <yamaguchi_shota@kakaku.com>
2019-11-05 14:16:54 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 山口 彰太 on 2019/11/05.
+//  Copyright © 2019 shouta.yamaguchi4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
